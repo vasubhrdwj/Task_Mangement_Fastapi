d898c6d7e6779d4d470d1f92f09a79b2291ebe3a
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-15 18:08:57 +0530

    Added gitignore

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,6 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
